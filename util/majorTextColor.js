7420f604292db5bf5d4dea8ea3935f7c1fa78b2d
KANGYONGSU23 <edc20203@gmail.com>
2024-03-05 14:39:44 +0900

    비전 부분 구현

---
+++ util/majorTextColor.js
@@ -0,0 +1,7 @@
+export const majorTextColor = {
+  "Front-end Engineer": "#FAFF00",
+  "Back-end Engineer": "#FF8A00",
+  "iOS Engineer": "#00E0FF",
+  "Android Engineer": "#00E741",
+  "UI/UX Designer": "#FF00F5",
+};
